573f0492323c0088e5cf3ee98acff4949d700f9f
edoardob90 <edoardob90@gmail.com>
2021-02-01 22:04:16 +0100

    Day 2 part 2

--- day_2.py
+++ day_2.py
@@ -9,8 +9,16 @@ import sys
 
 class Intcode():
     def __init__(self, init):
+        if not isinstance(init, list):
+            raise TypeError
         self.intcode = [int(x) for x in init]
+        self.idx = 0 # instruction pointer
+
+    def reset(self, init, noun=12, verb=2):
         self.idx = 0
+        self.intcode = [int(x) for x in init]
+        self.intcode[1] = noun
+        self.intcode[2] = verb
 
     def __len__(self):
         return len(self.intcode)
@@ -22,7 +30,10 @@ class Intcode():
         self.intcode[key] = value
 
     def __repr__(self):
-        return f'Intcode:\nIdx = {self.idx}\n{self.intcode}'
+        intcode_str = ''
+        for i,v in enumerate(self.intcode):
+            intcode_str += f'Intcode({i}) = {v}\n'
+        return f'Intcode:\nLast idx = {self.idx}\n{intcode_str}'
 
     def __iter__(self):
         return self
@@ -34,53 +45,56 @@ class Intcode():
         self.idx += 1
         return current
 
-    def jump(self, steps):
-        assert steps >= 0, "Step must be non-negative"
-        self.idx += steps
+    def jump(self, args):
+        assert len(args) >= 0, "Step must be non-negative"
+        self.idx += len(args)
 
-    def process(self, args):
-        assert len(args) == 4
+    def exec(self, args):
+        assert len(args) == 4, "Instructions length must be 4"
         opcode, addr = args[0], args[1:]
         if opcode == 1:
             self.intcode[addr[-1]] = self.intcode[addr[0]] + self.intcode[addr[1]]
+            return True
         elif opcode == 2:
             self.intcode[addr[-1]] = self.intcode[addr[0]] * self.intcode[addr[1]]
+            return True
         elif opcode == 99:
-            raise StopIteration
+            return False
         else:
             raise RuntimeError
 
+    def process(self):
+        status = True
+        while status and self.idx < len(self.intcode):
+            args = self.intcode[self.idx:self.idx + 4] # instruction arguments: 1 opcode + 3 parameters
+            status = self.exec(args)
+            self.output = self.intcode[0]
+            self.jump(args)
+
 if __name__ == '__main__':
-    intcode = Intcode(fileinput.input().readline().split(','))
+    intcode_str = fileinput.input().readline().split(',')
+    intcode = Intcode(intcode_str)
     #intcode = [int(x) for x in fileinput.input().readline().split(',')]
 
     # Part 1
-    intcode[1] = 12
-    intcode[2] = 2
+    intcode[1] = 12 # the "noun"
+    intcode[2] = 2 # the "verb"
 
-    ## Solution with my Intcode class
-    while intcode.idx < len(intcode):
-        args = intcode[intcode.idx:intcode.idx + 4]
-        try:
-            intcode.process(args)
-            intcode.jump(4)
-        except StopIteration:
-            break
-        except RuntimeError:
-            print(f'An error occurred!')
-            break
-
-    ## Solution with bare python
-    #idx = 0
-    #while idx < len(intcode):
-    #    code = intcode[idx]
-    #    if code == 99:
-    #        break
-    #    elif code == 1:
-    #        intcode[intcode[idx + 3]] = intcode[intcode[idx + 1]] + intcode[intcode[idx + 2]]
-    #    elif code == 2:
-    #        intcode[intcode[idx + 3]] = intcode[intcode[idx + 1]] * intcode[intcode[idx + 2]]
-    #    idx += 4
-
+    print("=======\nPart 1\n=======")
     print(intcode)
+    intcode.process()
+    print(intcode.output)
+
+    # Part 2
+    print("========\nPart 2\n========")
+
+    for noun in range(100):
+        for verb in range(100):
+            intcode.reset(intcode_str, noun, verb)
+            intcode.process()
+            if intcode[0] == 19690720:
+                print(intcode[0])
+                print(f'Noun: {intcode[1]}\nVerb: {intcode[2]}\nResult: {intcode[1] * 100 + intcode[2]}')
+                break
+
 
